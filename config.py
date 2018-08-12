b1e2360b718cb420f10e455a576243ad04fb2ade
Lee <15537186765@163.com>
2018-08-12 21:31:57 +0800

    抽取配置到配置文件

---
+++ config.py
@@ -0,0 +1,27 @@
+from redis import StrictRedis
+
+
+class Config(object):
+
+    DEBUG = True
+
+    # 配置mysql：指定数据库位置
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@mysql@127.0.0.1：3306/information_new'
+    # 禁用追踪mysql 因为mysql性能差，如果再去追踪mysql的所有修改 会再次浪费性能
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # redis 数据库配置
+    REDIS_HOST = '127.0.0.1'
+    REDIS_PORT = 6379
+
+    # 配置秘钥
+    SECRET_KEY = 'agshdjfk'
+
+    # 配置Session：：将flask中的session的数据引导到redis
+    SESSION_TYPE = 'redis'
+    # 配置redis的位置
+    SESSION_REDIS = StrictRedis(host = REDIS_HOST,port= REDIS_PORT)
+    # 使用签名将Session明文转成密文
+    SESSION_USE_SIGNER = True
+    # 设置session有效期: 这里指的是session的扩展操作session时设置的有效期 以秒为单位
+    PERMANENT_SESSION_LIFETIME = 60 * 60 * 24
--- manage.py
+++ manage.py
@@ -5,36 +5,36 @@ from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
 from flask_wtf import CSRFProtect
 from flask_session import Session # Session 和flask里的session不一样
-
+from config import Config
 
 # 创建app实例 __name__决定了如何查找静态文件
 app = Flask(__name__)
 
 
-class Config(object):
-
-    DEBUG = True
-
-    # 配置mysql：指定数据库位置
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@mysql@127.0.0.1：3306/information_new'
-    # 禁用追踪mysql 因为mysql性能差，如果再去追踪mysql的所有修改 会再次浪费性能
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # redis 数据库配置
-    REDIS_HOST = '127.0.0.1'
-    REDIS_PORT = 6379
-
-    # 配置秘钥
-    SECRET_KEY = 'agshdjfk'
-
-    # 配置Session：：将flask中的session的数据引导到redis
-    SESSION_TYPE = 'redis'
-    # 配置redis的位置
-    SESSION_REDIS = StrictRedis(host = REDIS_HOST,port= REDIS_PORT)
-    # 使用签名将Session明文转成密文
-    SESSION_USE_SIGNER = True
-    # 设置session有效期: 这里指的是session的扩展操作session时设置的有效期 以秒为单位
-    PERMANENT_SESSION_LIFETIME = 60 * 60 * 24
+# class Config(object):
+#
+#     DEBUG = True
+#
+#     # 配置mysql：指定数据库位置
+#     SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@mysql@127.0.0.1：3306/information_new'
+#     # 禁用追踪mysql 因为mysql性能差，如果再去追踪mysql的所有修改 会再次浪费性能
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+#
+#     # redis 数据库配置
+#     REDIS_HOST = '127.0.0.1'
+#     REDIS_PORT = 6379
+#
+#     # 配置秘钥
+#     SECRET_KEY = 'agshdjfk'
+#
+#     # 配置Session：：将flask中的session的数据引导到redis
+#     SESSION_TYPE = 'redis'
+#     # 配置redis的位置
+#     SESSION_REDIS = StrictRedis(host = REDIS_HOST,port= REDIS_PORT)
+#     # 使用签名将Session明文转成密文
+#     SESSION_USE_SIGNER = True
+#     # 设置session有效期: 这里指的是session的扩展操作session时设置的有效期 以秒为单位
+#     PERMANENT_SESSION_LIFETIME = 60 * 60 * 24
 # 配置redis
 redis_store = StrictRedis(host = Config.REDIS_HOST,port = Config.REDIS_PORT)
 
