30b295480ab692e526d15c19f8bca6152962ea07
Lee <15537186765@163.com>
2018-08-12 20:27:12 +0800

    配置Session，将flask session的数据存储到redis

--- manage.py
+++ manage.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
 from flask_wtf import CSRFProtect
-
+from flask_session import Session # Session 和flask里的session不一样
 
 # 创建app实例 __name__决定了如何查找静态文件
 app = Flask(__name__)
@@ -24,6 +24,14 @@ class Config(object):
     # 配置秘钥
     SECRET_KEY = 'agshdjfk'
 
+    # 配置Session：：将flask中的session的数据引导到redis
+    SESSION_TYPE = 'redis'
+    # 配置redis的位置
+    SESSION_REDIS = StrictRedis(host = REDIS_HOST,port= REDIS_PORT)
+    # 使用签名将Session明文转成密文
+    SESSION_USE_SIGNER = True
+    # 设置session有效期: 这里指的是session的扩展操作session时设置的有效期 以秒为单位
+    PERMANENT_SESSION_LIFETIME = 60 * 60 * 24
 # 配置redis
 redis_store = StrictRedis(host = Config.REDIS_HOST,port = Config.REDIS_PORT)
 
@@ -37,11 +45,18 @@ db = SQLAlchemy(app)
 # 什么情况下代表保护成功：如果用户发送POST，DELETE，PUT...时，没有携带csrf_token或者错误,服务器返回状态码400／403
 CSRFProtect(app)
 
+# 配置Session：将flask中的session的数据引导到redis
+Session(app)
+
 @app.route('/', methods = ['GET','POST'])
 def index():
 
     # 测试redis的写入
-    redis_store.set('name','xiaohua')
+    # redis_store.set('name','xiaohua')
+
+    # 回顾session缓存保持的数据
+    session['user_id'] = 1
+    session['user_name'] = 'xiaohua'
 
     return 'index'
 
