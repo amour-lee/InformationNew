8ec349ef903941bb864deea3675dfcd06b2aa724
Lee <15537186765@163.com>
2018-08-13 17:42:09 +0800

    日志封装

--- info/__init__.py
+++ info/__init__.py
@@ -11,22 +11,26 @@ from logging.handlers import RotatingFileHandler
 # 创建一个空的db对象
 db = SQLAlchemy()
 
-# 设置日志的记录等级
-logging.basicConfig(level=logging.DEBUG)  # 调试debug级
-# 创建日志记录器，指明日志保存的路径、每个日志文件的最大大小、保存的日志文件个数上限
-file_log_handler = RotatingFileHandler("logs/log", maxBytes=1024*1024*100, backupCount=10)
-# 创建日志记录的格式                 日志等级    输入日志信息的文件名 行数    日志信息
-formatter = logging.Formatter('%(levelname)s %(filename)s:%(lineno)d %(message)s')
-# 为刚创建的日志记录器设置日志记录格式
-file_log_handler.setFormatter(formatter)
-# 为全局的日志工具对象（flask app使用的）添加日志记录器
-logging.getLogger().addHandler(file_log_handler)
+def set_log(level):
+    # 设置日志的记录等级
+    logging.basicConfig(level=level)  # 调试debug级
+    # 创建日志记录器，指明日志保存的路径、每个日志文件的最大大小、保存的日志文件个数上限
+    file_log_handler = RotatingFileHandler("logs/log", maxBytes=1024*1024*100, backupCount=10)
+    # 创建日志记录的格式                 日志等级    输入日志信息的文件名 行数    日志信息
+    formatter = logging.Formatter('%(levelname)s %(filename)s:%(lineno)d %(message)s')
+    # 为刚创建的日志记录器设置日志记录格式
+    file_log_handler.setFormatter(formatter)
+    # 为全局的日志工具对象（flask app使用的）添加日志记录器
+    logging.getLogger().addHandler(file_log_handler)
 
 def create_app(config_name):
     """创建app的工厂方法"""
 
     # 读取配置类
     config_class = configs[config_name]
+
+    # 调用日志的配置方法
+    set_log(config_class.LOGGING_LEVEL)
 
     # 创建app实例 __name__决定了如何查找静态文件
     app = Flask(__name__)
